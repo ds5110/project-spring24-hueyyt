302fcc6d1f02b452bc3ae5ca7c0740c94356ffd4
yinqian1 <yin.qian1@northeastern.edu>
2024-04-21 21:46:02 -0400

    DDI map added

--- src/DDI_map.py
+++ src/DDI_map.py
@@ -4,35 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+# Read geographic data
 url = "https://www2.census.gov/geo/tiger/TIGER2022/TRACT/tl_2022_23_tract.zip"
-gdf = gpd.read_file(url)
+gdf = gpd.read_file(url, converters={"GEOID": int})
 gdf = gdf[gdf['ALAND'] > 0]
 
+# Read county-level administrative unit name data
 url = "https://www2.census.gov/geo/docs/reference/codes/files/st23_me_cousub.txt"
 df = pd.read_csv(url, header=None, converters={"county": str}, usecols=[2, 3], names=["county", "name"])
 df = df.drop_duplicates(subset="name")
 df.set_index('county', inplace=True)
 diction = df.to_dict(orient='index')
 
-df_data = pd.read_csv('data/SE.csv')
-df_data['county'] = df_data['id'].apply(lambda x: str(x)[2:5] + str(x)[-4:])
-df_data['GEOID'] = df_data['id'].astype(str)
-df_data.drop(columns=['id'], inplace=True)
-
-score = ['DDI_scaled']
+# Read INFA score data
+score = 'DDI_scaled'
 df_data = pd.read_csv('data/DDI.csv', index_col=None)
+df_data['id'] = df_data['id'].astype(str)
 
-del df_data[df_data.columns[0]]
+print(df_data.columns)
+
 df_data['county'] = df_data['id'].apply(lambda x: str(x)[2:5] + str(x)[-4:])
 df_data['title'] = df_data['county'].apply(lambda x: diction[x[:3]]['name'] + "\ntract:" + x[-4:] if x[:3] in diction else NA)
 df_data['title'] = df_data['title'] + "\n" + round(df_data[score], 2).astype(str)
 df_data['GEOID'] = df_data['id'].astype(str)
 df_data.drop(columns=['id'], inplace=True)
 
+# Merge geographic data and INFA scores
 new_gdf = gdf.merge(df_data[['GEOID', score, 'title']], on='GEOID', how='left')
 new_gdf.to_file('docs/'+score+'.json', driver="GeoJSON")
 print('Saving '+score+' GeoJSON data to docs/'+score+'.json')
 
+# Plot INFA score map
 new_wm = new_gdf.to_crs(epsg=3857)
 print('Plotting ' + score + ' scores on map')
 bounds = np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
